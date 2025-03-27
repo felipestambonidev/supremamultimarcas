0857abb10acbf35f392dea5b7cd1791184d3e04b
felipestambonidev <felipestambonii@gmail.com>
2025-03-26 22:10:58 -0300

    add: fiz o Header, falta algumas alterações

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,7 +1,38 @@
-export default function Header(){
-    return(
-        <div className="bg-amber-800 mx-20 mt-5">
-            <h1>Oi</h1>
-        </div>
-    )
-}
+"use client";
+
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import Image from "next/image";
+import Logo from "../../public/images/logosuprema/logosuprema.png";
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-[#3F3D3D] border border-[#FF4420]/20 rounded-full p-3 mx-4 md:mx-auto max-w-4xl mt-5 md:max-w-5xl">
+      <div className="flex justify-between items-center">
+        <Image src={Logo} alt="Logo Suprema" width={142} height={75} />
+        <nav className="hidden md:flex text-[22px] gap-16 text-white font-semibold pr-20">
+          <a href="/" className="hover:text-[#FF4420] duration-700">HOME</a>
+          <a href="#" className="hover:text-[#FF4420] duration-700">CARROS</a>
+          <a href="#" className="hover:text-[#FF4420] duration-700">QUEM SOMOS</a>
+        </nav>
+        <button className="md:hidden text-white text-3xl hover:text-[#FF4420] pr-5" onClick={() => setMenuOpen(!menuOpen)} aria-label={menuOpen ? "Fechar Menu" : "Abrir Menu"}>
+            {menuOpen ? <FiX /> : <FiMenu />}
+        </button>
+      </div>
+      <div className={`fixed top-0 right-0 w-3/4 max-w-sm h-full bg-[#3F3D3D]/80 backdrop-blur-md z-50 shadow-lg flex flex-col items-start p-5 transform transition-transform duration-500 ${ menuOpen ? "translate-x-0" : "translate-x-full" }`}>
+        <button className="text-white text-3xl hover:text-[#FF4420] self-end" onClick={() => setMenuOpen(false)} aria-label="Fechar Menu">
+            <FiX />
+        </button>
+        <nav className="mt-10 flex flex-col space-y-8 text-white font-semibold text-xl">
+            <a href="/" className="hover:text-[#FF4420] duration-700">HOME</a>
+            <a href="#" className="hover:text-[#FF4420] duration-700">CARROS</a>
+            <a href="#" className="hover:text-[#FF4420] duration-700">QUEM SOMOS</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
