e6b2b20bc64234c8c04b5c3b626854f719014643
felipestambonidev <felipestambonii@gmail.com>
2025-04-06 23:19:10 -0300

    add: criei a pagina de carros

---
+++ src/app/carros/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../../../components/header";
+
+export default function Carros(){
+    return(
+        <div>
+            <section>
+                <Header/>
+            </section>
+        </div>
+    )
+}
