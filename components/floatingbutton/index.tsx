7bcdf0be445fe033e09f480beea3da3575d782e7
felipestambonidev <felipestambonii@gmail.com>
2025-04-13 23:07:59 -0300

    add: criei o botao flutuante para o whatsapp

---
+++ components/floatingbutton/index.tsx
@@ -0,0 +1,14 @@
+import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
+import { links } from "../../constants/links";
+
+export default function WhatsAppButton() {
+  return (
+    <Link
+      href={links.whatsapp} className="fixed top-1/2 right-1 -translate-y-1/2 z-50">
+      <div className="bg-[#1E1E1E] rounded-2xl p-3 shadow-lg transition-transform hover:scale-110 border border-[#FF4420]/20">
+        <FaWhatsapp className="text-white text-3xl" />
+      </div>
+    </Link>
+  );
+}
