a5774583eadac9ff2d0c0495a2a7be7e26539a71
felipestambonidev <felipestambonii@gmail.com>
2025-04-21 23:34:19 -0300

    edit: atualizei o link do instagram nos links

--- constants/links.tsx
+++ constants/links.tsx
@@ -1,6 +1,6 @@
 export const links = {
     whatsapp: "https://wa.me/+5511947537444",
-    instagram: "https://www.instagram.com/seuPerfil",
+    instagram: "https://www.instagram.com/suprema_automoveis",
     localizacao: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.061335362582!2d-46.53984092376283!3d-23.45825195781399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef56adb55491b%3A0x768da3d7640a64d1!2sSuprema%20Autom%C3%B3veis!5e0!3m2!1spt-BR!2sbr!4v1744592211728!5m2!1spt-BR!2sbr",
     icarros: "https://supremaveiculos-vlhulda.icarros.com.br/ache/estoque.jsp?id=37191",
   };
