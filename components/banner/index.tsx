d4717b38f26773e20fb1b74e5f88dbd4d2e75321
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2025-04-22 14:15:46 -0300

    teste iphone

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -1,13 +1,25 @@
 "use client"
+import { useEffect, useState } from "react";
 
 import Filter from "../filter";
 
 export default function Banner() {
+
+
+  const [offsetY, setOffsetY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setOffsetY(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="relative h-[600px] overflow-hidden mt-5">
       <div
         className="absolute inset-0 z-[-1] brightness-30"
         style={{
+          transform: `translateY(${offsetY * 0.5}px)`,
           backgroundAttachment: "fixed",
           backgroundImage: "url('/images/banner/imagemfundo.png')",
           backgroundSize: "cover",
