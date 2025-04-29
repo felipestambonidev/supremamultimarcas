a6611b846fb9569a86e68968d7ea3c70fe59473d
felipestambonidev <felipestambonii@gmail.com>
2025-04-28 22:31:58 -0300

    edit: editei o estilo do floatbutton

--- components/floatingbutton/index.tsx
+++ components/floatingbutton/index.tsx
@@ -6,7 +6,7 @@ export default function WhatsAppButton() {
   return (
     <Link
       href={links.whatsapp} className="fixed top-1/2 right-1 -translate-y-1/2 z-50">
-      <div className="bg-[#1E1E1E] rounded-2xl p-3 shadow-lg transition-transform hover:scale-110 border border-[#FF4420]/20">
+      <div className="bg-[#14bb2a] rounded-full p-3 shadow-lg transition-transform hover:scale-110 border border-[#FF4420]/20">
         <FaWhatsapp className="text-white text-3xl" />
       </div>
     </Link>
