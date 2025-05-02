7f9943ff5de8a8900a0fbd925be7f45b2d96f1e6
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2025-05-02 15:50:38 -0300

    edit: fiz a parte responsiva do slide

--- components/slideslug/index.tsx
+++ components/slideslug/index.tsx
@@ -6,48 +6,50 @@ import Image from "next/image";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "./swiper-custom.css"; 
+import "./swiper-custom.css";
 
 export default function CarImageSlider({ carro }: { carro: Carro }) {
-  console.log("Imagens do carro: ", carro.images);  
-
   return (
-    <div className="car-slider-container">
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={11}
-        navigation
-        pagination={{ clickable: true }}
-        modules={[Navigation, Pagination]}
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-        }}
-        className="swiper-container"
-      >
-        {carro.images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <Image
-              src={image}
-              alt={`Imagem ${index + 1} de ${carro.model}`}
-              width={600}
-              height={200}
-              className="car-slide-image"
-              style={{ objectFit: "cover" }}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+      <div className="car-slider-container">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={11}
+          navigation
+          pagination={{ clickable: true }}
+          modules={[Navigation, Pagination]}
+          breakpoints={{
+            400: {
+              slidesPerView: 1,
+              spaceBetween: 90,
+            },
+            580: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 10,
+            },
+          }}
+          className="swiper-container"
+        >
+          {carro.images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={image}
+                alt={`Imagem ${index + 1} de ${carro.model}`}
+                width={600}
+                height={500}
+                className="car-slide-image"
+                style={{ objectFit: "cover" }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
   );
 }
