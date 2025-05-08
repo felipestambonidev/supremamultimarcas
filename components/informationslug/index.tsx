01a5e541b280e08ebb04bb4ea751a6c5d1678bec
felipestambonidev <felipestambonii@gmail.com>
2025-05-07 22:05:40 -0300

    edit: editei o novo estilo do slug, ainda nao acabei

--- components/informationslug/index.tsx
+++ components/informationslug/index.tsx
@@ -6,53 +6,59 @@ import { Carro } from "../../interfaces/Interface";
 
 export default function InformationsSlug({ carro }: { carro: Carro }) {
   return (
-    <div className="text-center uppercase max-w-md w-full mt-5 mx-auto px-4">
-      <section>
-        <h1 className="text-white text-3xl sm:text-4xl font-medium">
-          {carro.model}
-        </h1>
-        <h2 className="mt-1 text-gray-400 text-base sm:text-lg">
-          {carro.description}
-        </h2>
-      </section>
-
-      <section className="flex flex-col sm:flex-row justify-center items-center rounded-3xl p-4 mt-4 gap-4 sm:gap-5">
-        <div className="flex items-center gap-2">
-          <IoCalendar className="text-2xl sm:text-3xl text-white" />
-          <span className="text-white text-base sm:text-base font-medium">
-            {carro.year}
-          </span>
-        </div>
-        <div className="flex items-center gap-2">
-          <IoMdSpeedometer className="text-2xl sm:text-3xl text-white" />
-          <span className="text-white text-base sm:text-base font-medium lowercase">
-            {carro.km} Km
-          </span>
-        </div>
-        <div className="flex items-center gap-2">
-          <IoCarSportSharp className="text-2xl sm:text-3xl text-white" />
-          <span className="text-white text-base sm:text-base font-medium">
-            {carro.bodywork}
-          </span>
-        </div>
-      </section>
-
-      <div className="border-b border-gray-400 my-4" />
-
-      <div className="mt-4">
-        <h1 className="text-[#FF4420] text-3xl sm:text-5xl font-bold">
-          {carro.price}
-        </h1>
-      </div>
-
-      <div className="mt-4">
-        <Link
-          href={links.whatsapp}
-          className="bg-[#FF4420] text-white rounded-3xl p-2 px-4 mt-4 block text-center text-base sm:text-lg font-medium hover:bg-[#FF4420]/50 transition-colors duration-300"
-        >
-          FALE CONOSCO
-        </Link>
+    <div>
+      <div className="bg-[#3F3D3D] p-3 mx-4 lg:mx-auto max-w-5xl md:max-w-5xl uppercase text-white rounded-t-lg relative z-10 -mt-2">
+        <h1 className="text-3xl font-medium text-center sm:text-left">{carro.model}</h1>
       </div>
     </div>
   );
 }
+
+// <div className="text-center uppercase max-w-md w-full mt-5 mx-auto px-4">
+//   <section>
+//     <h1 className="text-white text-3xl sm:text-4xl font-medium">
+//       {carro.model}
+//     </h1>
+//     <h2 className="mt-1 text-gray-400 text-base sm:text-lg">
+//       {carro.description}
+//     </h2>
+//   </section>
+
+//   <section className="flex flex-col sm:flex-row justify-center items-center rounded-3xl p-4 mt-4 gap-4 sm:gap-5">
+//     <div className="flex items-center gap-2">
+//       <IoCalendar className="text-2xl sm:text-3xl text-white" />
+//       <span className="text-white text-base sm:text-base font-medium">
+//         {carro.year}
+//       </span>
+//     </div>
+//     <div className="flex items-center gap-2">
+//       <IoMdSpeedometer className="text-2xl sm:text-3xl text-white" />
+//       <span className="text-white text-base sm:text-base font-medium lowercase">
+//         {carro.km} Km
+//       </span>
+//     </div>
+//     <div className="flex items-center gap-2">
+//       <IoCarSportSharp className="text-2xl sm:text-3xl text-white" />
+//       <span className="text-white text-base sm:text-base font-medium">
+//         {carro.bodywork}
+//       </span>
+//     </div>
+//   </section>
+
+//   <div className="border-b border-gray-400 my-4" />
+
+//   <div className="mt-4">
+//     <h1 className="text-[#FF4420] text-3xl sm:text-5xl font-bold">
+//       {carro.price}
+//     </h1>
+//   </div>
+
+//   <div className="mt-4">
+//     <Link
+//       href={links.whatsapp}
+//       className="bg-[#FF4420] text-white rounded-3xl p-2 px-4 mt-4 block text-center text-base sm:text-lg font-medium hover:bg-[#FF4420]/50 transition-colors duration-300"
+//     >
+//       FALE CONOSCO
+//     </Link>
+//   </div>
+// </div>
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -22,19 +22,31 @@ export default async function CarDetailPage({
   return (
     <div>
       <header>
-        <Header />
-        <WhatsAppButton />
+        <Header/>
+        <WhatsAppButton  />
       </header>
-      <section className="mt-10 flex flex-col lg:flex-row items-center justify-center bg-[#3F3D3D]/20 shadow-2xl mx-4 lg:mx-12 p-5 rounded-3xl">
-        <PrincipalImageSlug carro={carro} />
-        <InformationsSlug carro={carro}  />
-      </section>
-      <section className="mx-12 mt-28">
+      <div className="sm:mt-10 mt-3">
         <CarImageSlider carro={carro} />
-      </section>
-      <footer>
-        <Footer/>
-      </footer>
+      </div>
+      <div>
+        <InformationsSlug carro={carro} />
+      </div>
     </div>
   );
 }
+// <div>
+//   <header>
+//     <Header />
+//     <WhatsAppButton />
+//   </header>
+//   <section className="mt-10 flex flex-col lg:flex-row items-center justify-center bg-[#3F3D3D]/20 shadow-2xl mx-4 lg:mx-12 p-5 rounded-3xl">
+//     <PrincipalImageSlug carro={carro} />
+//     <InformationsSlug carro={carro}  />
+//   </section>
+//   <section className="mx-12 mt-28">
+//     <CarImageSlider carro={carro} />
+//   </section>
+//   <footer>
+//     <Footer/>
+//   </footer>
+// </div>
