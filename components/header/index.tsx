98933c4dc36072e424782ca325ceb5cfa244c545
felipestambonidev <felipestambonii@gmail.com>
2025-05-07 21:48:03 -0300

    edit: personalizei o header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="bg-[#3F3D3D] border border-[#FF4420]/20 rounded-full p-3 mx-4 md:mx-auto max-w-4xl mt-5 md:max-w-5xl">
+    <header className="bg-[#3F3D3D] border border-[#FF4420]/20 rounded-full p-3 mx-4 lg:mx-auto max-w-4xl mt-5 md:max-w-5xl">
       <div className="flex justify-between items-center">
         <Link href="/"><Image src={Logo} alt="Logo Suprema" width={180} height={160} className="ml-5"/></Link>
         <nav className="hidden md:flex text-[22px] gap-16 text-white font-semibold pr-12">
