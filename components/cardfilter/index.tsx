14fc0be9bae1e818f547e20a5504146bf409f278
felipestambonidev <felipestambonii@gmail.com>
2025-05-11 12:53:00 -0300

    edit: editei a cor do card do filter

--- components/cardfilter/index.tsx
+++ components/cardfilter/index.tsx
@@ -5,8 +5,8 @@ export default function CardFilter() {
       <section className="flex items-center justify-center text-center">
         <div className="lg:text-5xl text-3xl font-medium mt-12 text-white bg-[#3F3D3D]/80 border border-[#FF4420]/20 p-5 mx-5 lg:mx-0 rounded-lg shadow-lg">
           <h1>BEM-VINDO A</h1>
-          <h1 className="text-red-600 mt-1">SUPREMA MULTIMARCAS</h1>
-          <p className="lg:text-2xl text-xl lg:mt-5 mt-2">MAIS DE <span className="text-red-600">40 ANOS</span> DE TRADIÇÃO!</p>
+          <h1 className="text-[#FF4420] mt-1">SUPREMA MULTIMARCAS</h1>
+          <p className="lg:text-2xl text-xl lg:mt-5 mt-2">MAIS DE <span className="text-[#FF4420]">40 ANOS</span> DE TRADIÇÃO!</p>
         </div>
       </section>
     );
