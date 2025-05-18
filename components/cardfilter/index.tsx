50324fc28299fce04f1835401c18f32ebef55f5b
felipestambonidev <felipestambonii@gmail.com>
2025-05-18 19:09:46 -0300

    add: adicionei a animação no texto do filtro

--- components/cardfilter/index.tsx
+++ components/cardfilter/index.tsx
@@ -1,14 +1,17 @@
-
+import { AnimatedSection } from "../ui/animated-section";
 
 export default function CardFilter() {
     return (
-      <section className="flex items-center justify-center text-center">
-        <div className="lg:text-5xl text-3xl font-medium mt-12 text-white bg-[#3F3D3D]/80 border border-[#FF4420]/20 p-5 mx-5 lg:mx-0 rounded-lg shadow-lg">
+      <AnimatedSection>
+        <section className="flex items-center justify-center text-center">
+        <div className="lg:text-5xl text-3xl font-bold mt-12 text-white  border-[#FF4420]/20 p-5 mx-5 lg:mx-0 rounded-lg shadow-lg">
           <h1>BEM-VINDO A</h1>
           <h1 className="text-[#FF4420] mt-1">SUPREMA MULTIMARCAS</h1>
           <p className="lg:text-2xl text-xl lg:mt-5 mt-2">MAIS DE <span className="text-[#FF4420]">40 ANOS</span> DE TRADIÇÃO!</p>
         </div>
       </section>
+      </AnimatedSection>
+      
     );
   }
   
