10a164346fcf69aada1cfe8b04c40e4ab4fe286c
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2025-07-05 22:01:30 -0300

    style: adicionei estilo novos de componentes na pagina slug

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,10 +3,9 @@ import { notFound } from "next/navigation";
 import Header from "../../../components/ui/header";
 import WhatsAppButton from "../../../components/ui/floatingbutton";
 import Footer from "../../../components/ui/footer";
-import Informations from "../../../components/ui/slug/informations";
 import CarImageSlider from "../../../components/ui/slug/SlideFromSlug";
-
-
+import RightColumn from "../../../components/ui/slug/RightColumn";
+import Informations from "../../../components/ui/slug/informations";
 
 export default async function CarDetailPage({
   params,
@@ -21,17 +20,27 @@ export default async function CarDetailPage({
   return (
     <div>
       <header>
-        <Header/>
-        <WhatsAppButton  />
+        <Header />
+        <WhatsAppButton />
       </header>
-      <div className="mt-5">
+      <div className="max-w-7xl mx-auto flex flex-col gap-6 px-4 lg:px-6">
         <Informations carro={carro} />
-      </div>
-      <div>
-         <CarImageSlider carro={carro} />
+        <div className="flex flex-col lg:flex-row gap-9">
+          <div className="flex-1">
+            <CarImageSlider carro={carro} />
+          </div>
+          <div className="w-full lg:w-[400px]">
+            <div className="hidden lg:block sticky top-24">
+              <RightColumn carro={carro} />
+            </div>
+            <div className="lg:hidden mt-8">
+              <RightColumn carro={carro} />
+            </div>
+          </div>
+        </div>
       </div>
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   );
