5497d723c512b34dc0e95a1e4303fbb1b825875d
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2025-07-21 13:08:37 -0300

    style: ajustei o brilho da foto paralax

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -16,7 +16,7 @@ export default function Banner() {
   return (
     <div className="relative h-[600px] overflow-hidden mt-5">
       <div
-        className="absolute inset-0 z-[-1] brightness-30"
+        className="absolute inset-0 z-[-1] brightness-21"
         style={{
           transform: `translateY(${offsetY * 0.5}px)`,
           backgroundAttachment: "fixed",
