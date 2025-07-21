187c16b3dcea3c1efaff4a4f012bfcd000a87e6a
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2025-07-21 13:15:36 -0300

    feat: adicionei o componente e o video na home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Footer from "../../components/ui/footer";
 import Header from "../../components/ui/header";
 import Highlights from "../../components/highligths";
 import ScrollToTopButton from "../../components/ui/scrolltopbutton/ScrollToTopButton";
+import VehicleCTA from "../../components/findyourcarsection";
 
 
 
@@ -20,7 +21,7 @@ export default function Home() {
         <Banner  />
         <ChooseUs />
         <Highlights />
-  
+        <VehicleCTA />
       </section>
       <footer>
         <Footer />
